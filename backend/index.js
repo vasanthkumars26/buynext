47e9a249473c83dd9e32dc17d7d18b2dee681c97
vasanthkumars26 <princevasanth754@gmail.com>
2025-10-07 18:08:52 +0530

    final22

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,10 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["buynext.vercel.app", "http://localhost:5173"],
+  credentials: true,
+}));
 
 
 mongoose
